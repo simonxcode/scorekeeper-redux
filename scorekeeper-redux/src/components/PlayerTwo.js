bf1c4c7f3f0781f0c1bfd9ec06146372b05cd3b8
Simon X <simonxiong11@gmail.com>
2019-09-11 19:33:46 -0700

    Increment player two's score

---
+++ scorekeeper-redux/src/components/PlayerTwo.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+//stateless function component
+function PlayerTwo(props) {
+  return (
+    <div>
+      <p>Player two {props.playerTwo}</p>
+      <button onClick={props.incrementPlayerTwo}>Increase Score</button>
+    </div>
+  );
+} 
+
+//function to dispatch action and action generator(event handler)
+function mapDispatchToProps(dispatch) {
+  return {
+     incrementPlayerTwo: () => {
+       console.log('incrementing player 2');
+       const action = {
+         type: 'INCREMENTTWO'
+       };
+       dispatch(action);
+     }
+  }
+}
+
+//function to display current state
+function mapStateToProp(state) {
+  console.log('player two score', state);
+  return {
+    playerTwo: state.playerTwo
+  }
+}
+
+export default connect(mapStateToProp, mapDispatchToProps)(PlayerTwo);
+
+
+
--- scorekeeper-redux/src/store/index.js
+++ scorekeeper-redux/src/store/index.js
@@ -3,7 +3,8 @@ import { createStore } from 'redux';
 //initial state of winning score, player 1 score and player 2 score
 const initialState = {
   count: 5,
-  playerOne: 0
+  playerOne: 0,
+  playerTwo: 0
 };
 
 //reducer function for updating object 
@@ -17,6 +18,8 @@ const reducer = (state = initialState, action) => {
       return Object.assign({}, state, {count: state.count - 1});
     case 'INCREMENTONE':
       return Object.assign({}, state, {playerOne: state.playerOne + 1});
+    case 'INCREMENTTWO':
+      return Object.assign({}, state, {playerTwo: state.playerTwo + 1});
     default: 
       return state;
   }

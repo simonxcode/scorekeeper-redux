79b49019fa22f5a21e472ccd03619565e9bf27b1
Simon X <simonxiong11@gmail.com>
2019-09-18 17:55:33 -0700

    separate reducers for players and winning score

--- scorekeeper-redux/src/store/index.js
+++ scorekeeper-redux/src/store/index.js
@@ -1,21 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 
-//initial state of winning score, player 1 score and player 2 score
-const initialState = {
-  count: 5,
+//initial state and player 1 score and player 2 score
+const playerDefaultState = {
   playerOne: 0,
   playerTwo: 0
 };
 
 //reducer function for updating object 
-const reducer = (state = initialState, action) => {
-  console.log('reducer running', action);
-
+const playerReducer = (state = playerDefaultState, action) => {
+  console.log('player reducer running', action);
   switch(action.type) {
-    case 'INCREMENT':
-      return Object.assign({}, state, {count: state.count + 1 });
-    case 'DECREMENT':
-      return Object.assign({}, state, {count: state.count - 1});
     case 'INCREMENTONE':
       return Object.assign({}, state, {playerOne: state.playerOne + 1});
     case 'INCREMENTTWO':
@@ -24,8 +18,37 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 }
-//redux state container
-const store = createStore(reducer);
+
+//initial state of winning score
+const winnerDefaultState = {
+  count: 5
+}
+
+//reducer function for updating winning score 
+const winnerReducer = (state = winnerDefaultState, action) => {
+  console.log('winner reducer running', action);
+  switch (action.type) {
+    case 'INCREMENT':
+      return Object.assign({}, state, {
+        count: state.count + 1
+      });
+    case 'DECREMENT':
+      return Object.assign({}, state, {
+        count: state.count - 1
+      });
+    default:
+      return state;
+  }
+}
+
+// redux state container
+const store = createStore(
+  combineReducers({
+    playerReducer,
+    winnerReducer
+  })
+);
+
 
 export default store;
 

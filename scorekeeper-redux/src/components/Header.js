d1e11ed36e97e8ad978482c0101f986d41fe49e9
Simon X <simonxiong11@gmail.com>
2019-11-09 15:07:51 -0800

    change color of some elements

--- scorekeeper-redux/src/components/Header.js
+++ scorekeeper-redux/src/components/Header.js
@@ -2,10 +2,8 @@ import React from 'react';
 
 const Header = (props) => (
   <div className='header'>
-    <div className='container'>
-      <h1 className='header__title'>{props.title}</h1>
-      {props.subtitle && <h2 className='header__subtitle'>{props.subtitle}</h2>}
-    </div>
+    <h1 className='header__title'>{props.title}</h1>
+    {props.subtitle && <h2 className='header__subtitle'>{props.subtitle}</h2>}
   </div>
 );
 

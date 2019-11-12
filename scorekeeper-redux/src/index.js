1412414772eb7c1eeb24883196329d384814d9ce
Simon X <simonxiong11@gmail.com>
2019-11-11 18:45:27 -0800

    fixed merge conflict

--- scorekeeper-redux/src/index.js
+++ scorekeeper-redux/src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
 import GameScoreComponent from './containers/GameScoreComponent';
@@ -31,9 +30,3 @@ render(
   document.getElementById('root')
 );
 
-=======
-import ReactDOM from 'react-dom';
-import WinningScore from './actions/WinningScore';
-
-ReactDOM.render( < WinningScore / > , document.getElementById('root'));
->>>>>>> master
